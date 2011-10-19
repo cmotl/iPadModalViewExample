30eefd5063bcf7cb490d678e18157d99e953cd0f
Christopher Motl <cmotl@cmotl.com>
2011-10-19 17:12:24 -0500

    Initial Commit

---
+++ iPadModalViewExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iPadModalViewExample
+//
+//  Created by Christopher Motl on 10/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
